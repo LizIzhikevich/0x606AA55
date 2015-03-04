a4c81e761b4a80a23609f3d3c7065c357356fb0e
JOANNE YEH <joanney3h@yahoo.com>
2015-03-04 02:06:38 -0800

    - Fixed up the csv_parser to be much more nice and reusable - updated csv_reader to be more reusable

--- csv_parser.py
+++ csv_parser.py
@@ -1,25 +1,29 @@
 __author__ = 'joanneyeh'
 
-import csv
 import pandas as pd
 
-def parse_text_file(filename):
+"""
+This program converts the text files for the California
+Discipline Dataset to a csv file for easy processing
+
+Usage examples at the end of file
+
+"""
+
+
+def parse_text_file(filename, output_file_name):
     with open(filename) as file:
         without_commas = file.read().replace(',', '')
         without_commas = without_commas.replace('*', '0')
-    test = without_commas.split('\n')
-    return test
+    file = without_commas.split('\n')
+    data = []
+    for entry in file:
+        data.append(entry.split('\t'))
+    df = pd.DataFrame(data)
+    df = df.drop([df.columns[13]], axis=1)
+    df = df.drop([df.columns[13]], axis=1)
+    df.to_csv(output_file_name, index=False, header=False)
+    return
 
-tester = parse_text_file('CAdiscipline14.txt')
-
-data = []
-for entry in tester:
-    data.append(entry.split('\t'))
-
-df = pd.DataFrame(data)
-df = df.drop([df.columns[13]], axis=1)
-df = df.drop([df.columns[13]], axis=1)
-
-df.to_csv('CA_discip14.csv', index=False, header=False)
-
-DataFrame.plot()
+parse_text_file('CA_discip13.txt', 'CA_discip13.csv')
+parse_text_file('CA_discip12.txt', 'CA_discip13.csv')
--- csv_reader.py
+++ csv_reader.py
@@ -1,34 +1,46 @@
 __author__ = 'joanneyeh'
 import pandas as pd
 
+"""
+This program will return subsets of each dataset and
+concatenate corresponding subsets together.
+Usage:
+
+"""
+
 
 def read_in_file(filename):
     data = pd.read_csv(filename)
     return data
 
+"""
+Methods to print out list of unique values in a column (for sanity check):
 
-""" MAIN METHOD """
-
-ca14 = read_in_file('CA_discip14.csv')
-ca15 = read_in_file('')
-# save column titles to list
 column_names = list(ca14)
-print(column_names)
 
-# list unique values in column 0
 agg_levels = pd.unique(ca14[column_names[0]])
 names = pd.unique(ca14[column_names[2]])
 disc_type = pd.unique(ca14[column_names[3]])
 list_ethnic = pd.unique(ca14[column_names[4]])
 year = pd.unique(ca14[column_names[5]])
+"""
 
-# print(ca14)
 
-
-def subset_by_level(dataframe):
-    county_data = dataframe[dataframe.AggregateLevel == 'O']
-    district_data = dataframe[dataframe.AggregateLevel == 'D']
-    school_data = dataframe[dataframe.AggregateLevel == 'S']
-    state_data = dataframe[dataframe.AggregateLevel == 'T']
+def subset_by_level(df):
+    county_data = df[df.AggegateLevel == 'O']
+    district_data = df[df.AggegateLevel == 'D']
+    school_data = df[df.AggegateLevel == 'S']
+    state_data = df[df.AggegateLevel == 'T']
     return county_data, district_data, school_data, state_data
+
+""" MAIN METHOD HERE """
+
+ca14 = read_in_file('CA_discip14.csv')
+country_data14, district_data14, school_data14, state_data14 = subset_by_level(ca14)
+
+ca13 = read_in_file('CA_discip13.csv')
+country_data13, district_data13, school_data13, state_data13 = subset_by_level(ca13)
+
+ca12 = read_in_file('CA_discip12.csv')
+country_data12, district_data12, school_data12, state_data12 = subset_by_level(ca12)
 
